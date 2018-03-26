645458421528e67f753d0b5baf118746aa482bc1
AnthonyChen7 <hp_a1700@hotmail.com>
2018-03-25 19:07:20 -0700

    forgot to get everything

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello!
+      </div>
+    );
+  }
+}
+
+export default App;
