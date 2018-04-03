7268be594761cd95ed990273ba45b6838e41be4e
AnthonyChen7 <hp_a1700@hotmail.com>
2018-04-02 18:19:01 -0700

    stubbed in side bar

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,13 @@ import './App.css';
 import { HeaderBar } from './widgets/components/header-bar/header-bar';
 import { AboutMe } from './widgets/components/about-me/about-me';
 import { Footer } from './widgets/components/footer/footer';
-// import { SideBar } from './widgets/components/side-bar/side-bar';
+import { SideBar } from './widgets/components/side-bar/side-bar';
 
 class App extends React.Component {
   render() {
+    // don't apply styles because here is always rendered first
+    // note that header isn't stickied
+    // note that style can't be applied to react component, so wrap them inside div and then apply
     return (
       <div className="app-style">
         <div>
@@ -14,7 +17,13 @@ class App extends React.Component {
         </div>
 
         <div className="content">
-          <AboutMe />
+          <div>
+            <SideBar />
+          </div>
+
+          <div>
+            <AboutMe />
+          </div>
         </div>
 
         <div className="footer">
--- src/widgets/components/side-bar/side-bar.tsx
+++ src/widgets/components/side-bar/side-bar.tsx
@@ -2,6 +2,14 @@ import * as React from 'react';
 import './side-bar.css';
 export class SideBar extends React.Component {
   render () {
-    return (<div>I am a sidebar</div>);
+    return (
+    <div>
+      <ul>
+        <li>About Me</li>
+        <li>Work Experience</li>
+        <li>Projects</li>
+        <li>Hobbies</li>
+      </ul>
+    </div>);
   }
 }
