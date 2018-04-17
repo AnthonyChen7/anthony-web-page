6c8225c4ccd32725527d92904efd8978f57e06cc
AnthonyChen7 <hp_a1700@hotmail.com>
2018-04-16 20:18:40 -0700

    imported bootstrap

--- src/widgets/components/about-me/about-me.tsx
+++ src/widgets/components/about-me/about-me.tsx
@@ -1,16 +1,18 @@
 import * as React from 'react';
 import './about-me.css';
-
+import { Jumbotron } from 'react-bootstrap';
 export class AboutMe extends React.Component {
   render () {
     return (
-      <div>
-        <p>Hi my name is Anthony Chen and welcome to my website!</p>
-        <p>I graduated from UBC in 2017 with a bachelor's degree in Computer Science & Statistics.</p>
-        <p>Right now, at this stage in my life, I'm interested in specializing and pursuing a career as a front end software engineer.</p>
+      // <div>
+        <Jumbotron>
+          <p>Hi my name is Anthony Chen and welcome to my website!</p>
+          <p>I graduated from UBC in 2017 with a bachelor's degree in Computer Science & Statistics.</p>
+          <p>Right now, at this stage in my life, I'm interested in specializing and pursuing a career as a front end software engineer.</p>
 
-        <p>Why pursue only a career in front end software development?</p>
-      </div>
+          <p>Why pursue only a career in front end software development?</p>
+        </Jumbotron>
+      // </div>
     );
   }
 }
