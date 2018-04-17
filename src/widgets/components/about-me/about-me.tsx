95fc233085b20d55352bdfc2ad7a76174db73a46
AnthonyChen7 <hp_a1700@hotmail.com>
2018-04-16 19:37:55 -0700

    Began writing up content

--- src/widgets/components/about-me/about-me.tsx
+++ src/widgets/components/about-me/about-me.tsx
@@ -5,25 +5,11 @@ export class AboutMe extends React.Component {
   render () {
     return (
       <div>
-        Lorem ipsum dolor sit amet, cum feugait electram id. Ei pro commodo luptatum partiendo, eirmod veritus mandamus qui te, duo et alii denique. Ut unum euismod duo. Ut summo dissentias vim, adhuc veniam assentior mel ne, quo id efficiantur signiferumque.
+        <p>Hi my name is Anthony Chen and welcome to my website!</p>
+        <p>I graduated from UBC in 2017 with a bachelor's degree in Computer Science & Statistics.</p>
+        <p>Right now, at this stage in my life, I'm interested in specializing and pursuing a career as a front end software engineer.</p>
 
-        Causae consequat id mei. At duo graece semper. Sit vivendum explicari at. Cu legere ignota assentior quo, ne vix audiam discere rationibus, qui cu vero mentitum iudicabit. Sint percipit theophrastus his id, discere referrentur qui ne.
-
-        Quo quis debitis nonumes ei. Has mandamus mediocritatem at, no has mnesarchum percipitur assueverit. Vitae utamur latine ut eam, falli accusata ius an. Quot nemore quo et, pro verterem repudiandae te. Nisl diam salutatus sit at, fierent lobortis consetetur nam te. Zril patrioque id usu.
-
-        In vel ludus nemore gloriatur, doctus aeterno pericula qui ad. Eu est nemore detracto. Ut nisl complectitur pro, tollit fabulas mei ne, atomorum salutatus honestatis mel ei. No partem admodum mediocrem cum, quo ei agam paulo audiam, illud postulant usu ei. Mea ei probatus convenire, ferri repudiandae vis no, mea possim tritani ex. Ea fabulas repudiare expetendis sit.
-
-        Usu ne inermis mandamus, malorum constituto no eum. Vitae cetero eos ut, vel eros consulatu percipitur id, pri te graeci volumus volutpat. An sit dicant rationibus voluptatibus, sed primis option numquam no. Te velit invidunt accusamus mea. Magna laudem commune has ex. Cu deleniti urbanitas est, sit at oratio nostrud appareat. Denique repudiare quo eu, omnis errem prompta quo eu, pri oratio dolorem id.
-
-        Lorem ipsum dolor sit amet, cum feugait electram id. Ei pro commodo luptatum partiendo, eirmod veritus mandamus qui te, duo et alii denique. Ut unum euismod duo. Ut summo dissentias vim, adhuc veniam assentior mel ne, quo id efficiantur signiferumque.
-
-        Causae consequat id mei. At duo graece semper. Sit vivendum explicari at. Cu legere ignota assentior quo, ne vix audiam discere rationibus, qui cu vero mentitum iudicabit. Sint percipit theophrastus his id, discere referrentur qui ne.
-
-        Quo quis debitis nonumes ei. Has mandamus mediocritatem at, no has mnesarchum percipitur assueverit. Vitae utamur latine ut eam, falli accusata ius an. Quot nemore quo et, pro verterem repudiandae te. Nisl diam salutatus sit at, fierent lobortis consetetur nam te. Zril patrioque id usu.
-
-        In vel ludus nemore gloriatur, doctus aeterno pericula qui ad. Eu est nemore detracto. Ut nisl complectitur pro, tollit fabulas mei ne, atomorum salutatus honestatis mel ei. No partem admodum mediocrem cum, quo ei agam paulo audiam, illud postulant usu ei. Mea ei probatus convenire, ferri repudiandae vis no, mea possim tritani ex. Ea fabulas repudiare expetendis sit.
-
-        Usu ne inermis mandamus, malorum constituto no eum. Vitae cetero eos ut, vel eros consulatu percipitur id, pri te graeci volumus volutpat. An sit dicant rationibus voluptatibus, sed primis option numquam no. Te velit invidunt accusamus mea. Magna laudem commune has ex. Cu deleniti urbanitas est, sit at oratio nostrud appareat. Denique repudiare quo eu, omnis errem prompta quo eu, pri oratio dolorem id.
+        <p>Why pursue only a career in front end software development?</p>
       </div>
     );
   }
